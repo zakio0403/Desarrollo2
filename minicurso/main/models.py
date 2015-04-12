1f1fcfce174033c9a9850be51356d9e78f8f215d
Mauro Castillo <mauricio.mina@correounivalle.edu.co>
2015-04-12 16:41:37 -0500

    Creacion de modelo basico
    Creacion de Modelo basico

--- minicurso/main/models.py
+++ minicurso/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class departamento(models.Model):
+	#Creacion de la tabla de departamentos
+	id =  models.AutoField(primary_key=True)
+	nombre = models.CharField(max_length = 30)
+
+class municipio(models.Model):
+	#Creacion de la tabla de municipis
+	id =  models.AutoField(primary_key=True)
+	nombreMunicipio = models.CharField(max_length=30)
+	iddepartamento = models.IntegerField() #Creo llave foranea a departamento
+
+class Person(models.Model):
+	#indentificador primario base de datos
+	id = models.AutoField(primary_key=True)
+	numberdocument = models.CharField(max_length = 30)
+   	first_name = models.CharField(max_length = 30)
+	last_name = models.CharField(max_length = 30)
+	birthdate = models.DateTimeField()
+	departemento = models.IntegerField() # creo llave foranea a departamento
+
