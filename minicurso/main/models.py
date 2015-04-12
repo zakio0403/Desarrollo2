e08fd05a81ceceeb481f98f86a49f14b493c0390
Mauro Castillo <mauricio.mina@correounivalle.edu.co>
2015-04-12 18:27:30 -0500

    Modificacion para agregar Templete
    Modificaciones realizadas por el control y pruebas de Template

--- minicurso/main/models.py
+++ minicurso/main/models.py
@@ -1,13 +1,18 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
-
-class departamento(models.Model):
+#Construido Por Mauro Castillo 
+#Lo construi para realizar pruebas con los Temples
+class BodyPanelAdmin(models.Model):
+#Esta clase contiene la forma como sera mostrada la pantalle principal a los usuarios
+	Title = models.CharField(max_length = 30)
+	Body = models.TextField()
+class Departamento(models.Model):
 	#Creacion de la tabla de departamentos
 	id =  models.AutoField(primary_key=True)
 	nombre = models.CharField(max_length = 30)
 
-class municipio(models.Model):
+class Municipio(models.Model):
 	#Creacion de la tabla de municipis
 	id =  models.AutoField(primary_key=True)
 	nombreMunicipio = models.CharField(max_length=30)
@@ -21,4 +26,13 @@ class Person(models.Model):
 	last_name = models.CharField(max_length = 30)
 	birthdate = models.DateTimeField()
 	departemento = models.IntegerField() # creo llave foranea a departamento
+
+class MainPost(admin.ModelAdmin):
+	lis_display = ('Title','Body') 
+
+#Agrego el modelo al panel de administracion
+admin.site.register(Departamento)
+admin.site.register(Municipio)
+admin.site.register(BodyPanelAdmin, MainPost)
+admin.site.register(Person)
 
